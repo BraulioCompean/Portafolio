29b018e3fa65aa3accfa8019ff1c9a926a48f303
ghstll <22690128@tecvalles.mx>
2024-11-16 09:36:45 -0600

    PokeAPI agregada

--- practicas/scripts/pokeapi.js
+++ practicas/scripts/pokeapi.js
@@ -1,54 +1,71 @@
-let nombreTxt = document.getElementById("pokemon-name")
-let typesList = document.getElementById("pokemon-types")
-let image = document.getElementById("pokemon-image")
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonImage = document.getElementById("pokemon-image");
+const pokemonWeight = document.getElementById("pokemon-weight");
+const pokemonHeight = document.getElementById("pokemon-height");
+const containerTypes = document.getElementById("container-types");
+const pokemonCard = document.getElementById("card-pokemon");
+const audioPokemon = document.getElementById("audio-pokemon")
+const pokemonBaseStats = document.querySelectorAll(".h3-base-stat");
+
+function handleInputChange(event) {
+    const inputValor = event.target.value;
+    buscar(inputValor);
+}
+
+function buscar(nombre) {
+    const nombreFormateado = nombre.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nombreFormateado}`)
+        .then((response) => {
+            if (response.status == 404) {
+                throw new Error("Pokemon no encontrado");
+            } else {
+                return response.json();
+            }
+        })
+        .then((pokemon) => {
+            if (pokemon !== undefined && nombre) {
+                console.log(pokemon);
+                containerTypes.innerHTML = "";
+                pokemonName.innerText =
+                    pokemon.name.charAt(0).toUpperCase() +
+                    pokemon.name.slice(1);
+                pokemonImage.setAttribute(
+                    "src",
+                    pokemon.sprites.other["official-artwork"].front_default
+                );
+                pokemonWeight.innerText =
+                    Math.round(pokemon.weight * 0.1) + " Kg";
+                pokemonHeight.innerText =
+                    Math.round(pokemon.height * 0.1) + " M";
 
 
 
-// for (let i = 1; i < 50; i++) {
 
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-//     .then((response) => response.json())
-//     .then((pokemon) => {
-//         // console.log(pokemon.name)
-//         console.log(pokemon.types[0].type.name)
-//         // console.log(pokemon.sprites.front_default)
-//         nombreTxt.innerText = pokemon.name
-//         let lista = document.createElement("ul")
-//         typesList.appendChild(lista)
-//         pokemon.types.forEach(element =>{
-//             let nombre = element.type.name
-//             let li = document.createElement("li")
-//             li.innerHTML = nombre
-//             lista.appendChild(li)            
+                pokemon.types.forEach((element) => {
+                    const typeContainer = document.createElement("div");
+                    typeContainer.className = "element-types";
+                    typeContainer.style.backgroundColor = `var(--${element.type.name})`;
+                    const type = document.createElement("h3");
+                    type.className = "h3-type-element";
+                    type.innerText =
+                        element.type.name.charAt(0).toUpperCase() +
+                        element.type.name.slice(1);
 
-//         })
-//         image.setAttribute("src",pokemon.sprites.front_default)
-      
-//     })
-//     .catch((error) => console.error(error))
-    
-// }
+                    typeContainer.appendChild(type);
+                    containerTypes.appendChild(typeContainer);
+                    pokemonCard.style.backgroundColor = `var(--${element.type.name})`;
+                });
 
-// Prueba for
-
-
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-.then((response) => response.json())
-.then((pokemon) => {
-    // console.log(pokemon.name)
-    console.log(pokemon.types[0].type.name)
-    // console.log(pokemon.sprites.front_default)
-    nombreTxt.innerText = pokemon.name
-    let lista = document.createElement("ul")
-    typesList.appendChild(lista)
-    pokemon.types.forEach(element =>{
-        let nombre = element.type.name
-        let li = document.createElement("li")
-        li.innerHTML = nombre
-        lista.appendChild(li)            
-
-    })
-    image.setAttribute("src",pokemon.sprites.front_default)
-  
-})
-.catch((error) => console.error(error))
+                for (let i = 0; i < pokemonBaseStats.length; i++) {
+                    const element = pokemonBaseStats[i];
+                    element.innerText = pokemon.stats[i].stat.name.toUpperCase() + " : " + pokemon.stats[i].base_stat
+                    
+                }
+                audioPokemon.setAttribute("src",pokemon.cries.latest)
+                audioPokemon.volume = 0.1
+            }
+        })
+        .catch((error) => {
+            window.alert(error.message);
+        });
+}
