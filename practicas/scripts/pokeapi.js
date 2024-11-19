9ce8ffad34873c6ecb9929ad22e2c823bd83e16a
ghstll <22690128@tecvalles.mx>
2024-11-18 19:47:05 -0600

    function modificada

--- practicas/scripts/pokeapi.js
+++ practicas/scripts/pokeapi.js
@@ -4,45 +4,48 @@ const pokemonWeight = document.getElementById("pokemon-weight"); // OBTENEMOS EL
 const pokemonHeight = document.getElementById("pokemon-height"); // OBTENEMOS EL ELEMENTO dd QUE MOSTRARA LA ALTURA DEL POKEMON
 const containerTypes = document.getElementById("container-types"); //OBTENEMOS EL CONTENEDOR DIV DE LOS TIPOS QUE TIENE EL POKEMON PARA ASI PODER AÑADIRSELOS DESPUES
 const pokemonCard = document.getElementById("card-pokemon"); // OBTENEMOS EL CONTENEDOR PRINCIPAL DE LA CARTA DEL POKEMON PARA PODER DARLE ESTILOS MAS TARDE
-const audioPokemon = document.getElementById("audio-pokemon") // OBTENEMOS EL ELEMENTO audio PARA INSERTAR EL AUDIO DEL POKEMON
+const audioPokemon = document.getElementById("audio-pokemon"); // OBTENEMOS EL ELEMENTO audio PARA INSERTAR EL AUDIO DEL POKEMON
 const pokemonBaseStats = document.querySelectorAll(".h3-base-stat"); //POKEMON OBTENEMOS TODOS LOS ELEMENTOS h3 QUE TENGAN LA CLASE ".h3-base-start" Y LOS GUARDAREMOS COMO UN ARRAY
 
-function handleInputChange(event) {  //FUNCION PARA OBTENER EL ID O NOMBRE DEL POKEMON QUE SE HA INTRODUCIDO EN EL INPUT
+function handleInputChange(event) {
+    //FUNCION PARA OBTENER EL ID O NOMBRE DEL POKEMON QUE SE HA INTRODUCIDO EN EL INPUT
     const inputValor = event.target.value;
-    buscar(inputValor);
+    buscarPokemon(inputValor);
 }
 
-function buscar(nombre) { //FUNCION QUE SE EJECUTARA CADA VEZ QUE HAYA UN CAMBIO EN EL INPUT
+function buscarPokemon(nombre) {
+    //FUNCION QUE SE EJECUTARA CADA VEZ QUE HAYA UN CAMBIO EN EL INPUT
     const nombreFormateado = nombre.toLowerCase(); // FORMATEAMOS EL NOMBRE QUE RECIBE LA FUNCION PARA QUE PUEDA ACEPTAR TANTO MAYUSCULAS COMO MINUSCULAS
     fetch(`https://pokeapi.co/api/v2/pokemon/${nombreFormateado}`) //HACEMOS EL LLAMADO A LA API
         .then((response) => {
-            if (response.status == 404) { //EN CASO DE QUE EL ESTADO DE LA RESPUESTA SEA 404, LO CUAL SIGNIFICA QUE EL ID O NOMBRE DEL POKEMON NO ES VALIDO, ARROJAREMOS UN ERROR
+            if (response.status == 404) {
+                //EN CASO DE QUE EL ESTADO DE LA RESPUESTA SEA 404, LO CUAL SIGNIFICA QUE EL ID O NOMBRE DEL POKEMON NO ES VALIDO, ARROJAREMOS UN ERROR
                 throw new Error("Pokemon no encontrado");
             } else {
                 return response.json(); //DE LO CONTRARIO, REGRESAREMOS LA RESPUESTA EN FORMATO JSON
             }
         })
         .then((pokemon) => {
-            if (pokemon !== undefined && nombre) { //VERIFICAMOS QUE LA RESPUESTA NO SEA UNDEFINED Y QUE EXISTE UN NOMBRE
-                console.clear()
+            if (pokemon !== undefined && nombre) {
+                //VERIFICAMOS QUE LA RESPUESTA NO SEA UNDEFINED Y QUE EXISTE UN NOMBRE
+                console.clear();
                 console.log(pokemon);
                 containerTypes.innerHTML = ""; //RESETEAMOS EL CONTENEDOR DE TIPOS DEL POKEMON
-                pokemonName.innerText =         //AGREGAREMOS EL NOMBRE DEL POKEMON AL ELEMENTO h1 = (pokemonName)
+                pokemonName.innerText = //AGREGAREMOS EL NOMBRE DEL POKEMON AL ELEMENTO h1 = (pokemonName)
                     pokemon.name.charAt(0).toUpperCase() + //CONVERTIMOS EL PRIMER CARACTER A MAYUSCULAS Y CONCATENAMOS CON EL RESTO DE LA CADENA
                     pokemon.name.slice(1);
-                pokemonImage.setAttribute(      // ACTUALIZAMOS LA FUENTE DE LA IMAGEN QUE USAREMOS (SRC) CON EL NUEVO LINK
+                pokemonImage.setAttribute(
+                    // ACTUALIZAMOS LA FUENTE DE LA IMAGEN QUE USAREMOS (SRC) CON EL NUEVO LINK
                     "src",
                     pokemon.sprites.other["official-artwork"].front_default
                 );
-                pokemonWeight.innerText =                               //FORMATEAMOS LOS DATOS QUE NOS DA LA API
+                pokemonWeight.innerText = //FORMATEAMOS LOS DATOS QUE NOS DA LA API
                     Math.round(pokemon.weight * 0.1) + " Kg";
                 pokemonHeight.innerText =
                     Math.round(pokemon.height * 0.1) + " M";
 
-
-
-
-                pokemon.types.forEach((element) => {                    //RECORREMOS EL ARRAY QUE NOS DA LA API, POR CADA TIPO CREAREMOS SU RESPECTIVA TARJETA
+                pokemon.types.forEach((element) => {
+                    //RECORREMOS EL ARRAY QUE NOS DA LA API, POR CADA TIPO CREAREMOS SU RESPECTIVA TARJETA
                     const typeContainer = document.createElement("div");
                     typeContainer.className = "element-types";
                     typeContainer.style.backgroundColor = `var(--${element.type.name})`;
@@ -59,11 +62,13 @@ function buscar(nombre) { //FUNCION QUE SE EJECUTARA CADA VEZ QUE HAYA UN CAMBIO
 
                 for (let i = 0; i < pokemonBaseStats.length; i++) {
                     const element = pokemonBaseStats[i];
-                    element.innerText = pokemon.stats[i].stat.name.toUpperCase() + " : " + pokemon.stats[i].base_stat
-                    
+                    element.innerText =
+                        pokemon.stats[i].stat.name.toUpperCase() +
+                        " : " +
+                        pokemon.stats[i].base_stat;
                 }
-                audioPokemon.setAttribute("src",pokemon.cries.latest) //AGREMAMOS LA FUENTE DEL SONIDO AL ELEMENTO audio = audioPokemon
-                audioPokemon.volume = 0.1 //SETEAMOS EL VOLUMEN A 0.1 PARA QUE NO ESTE AL MAXIMO
+                audioPokemon.setAttribute("src", pokemon.cries.latest); //AGREMAMOS LA FUENTE DEL SONIDO AL ELEMENTO audio = audioPokemon
+                audioPokemon.volume = 0.1; //SETEAMOS EL VOLUMEN A 0.1 PARA QUE NO ESTE AL MAXIMO
             }
         })
         .catch((error) => {
